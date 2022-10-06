23c96b27bd950258961ca14f2e248b52533774ad
Felipe Treis <felipetreis1@gmail.com>
2022-10-06 17:02:20 -0300

    Created urls to search, category, brand and item

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,8 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('store/search/', views.search, name='search'),
+    path('store/category/<int:category>/', views.category, name='category'),
+    path('store/brand/<int:brand>/', views.brand, name='brand'),
+    path('store/<int:id>/', views.item, name='item'),
 ]
