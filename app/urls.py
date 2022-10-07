f7f05565700912735684f531fced4efc8ee36c03
Felipe Treis <felipetreis1@gmail.com>
2022-10-07 11:15:17 -0300

    Added id in brand and category

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ app_name = 'app'
 urlpatterns = [
     path('', views.home, name='home'),
     path('store/search/', views.search, name='search'),
-    path('store/category/<int:category>/', views.category, name='category'),
-    path('store/brand/<int:brand>/', views.brand, name='brand'),
+    path('store/category/<int:category_id>/', views.category, name='category'),
+    path('store/brand/<int:brand_id>/', views.brand, name='brand'),
     path('store/<int:id>/', views.item, name='item'),
 ]
